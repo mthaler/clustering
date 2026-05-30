caf238a45757833351b57709ca252ae9ad6d8dd5
Michael Thaler <michael.thaler2@gmail.com>
2026-05-30 11:06:03 +0200

    Updated main.go.

--- main.go
+++ main.go
@@ -7,11 +7,41 @@ import (
 	"math"
 	"math/rand"
 	"os"
+	"strconv"
 )
 
 func main() {
 	faithful := readCsvFile("faithful.csv")
-	fmt.Printf("%d\n", len(faithful))
+	var points []point
+	for _, v := range faithful {
+		// only add a point if this is not a line with labels
+		if v[0] != "eruptions" {
+			x, err := strconv.ParseFloat(v[0], 64)
+			if err != nil {
+				log.Fatal("Could not parse number of eruptions")
+			}
+			y, err := strconv.ParseFloat(v[1], 64)
+			if err != nil {
+				log.Fatal("Could not parse waiting time")
+			}
+			p := point{x: x, y: y}
+			points = append(points, p)
+		}
+	}
+	maxErruptions := 0.0
+	for _, p := range points {
+		if p.x > maxErruptions {
+			maxErruptions = p.x
+		}
+	}
+	maxWaiting := 0.0
+	for _, p := range points {
+		if p.y > maxWaiting {
+			maxWaiting = p.y
+		}
+	}
+	centroids := generateCentroids(2, maxErruptions, maxWaiting)
+	fmt.Printf("%+v\n", centroids)
 }
 
 func readCsvFile(filePath string) [][]string {
@@ -36,11 +66,11 @@ func distance(p1 point, p2 point) float64 {
 }
 
 // generates k centroids
-func generateCentroids(k int) []centroid {
+func generateCentroids(k int, maxX float64, maxY float64) []centroid {
 	var c []centroid
 	c = make([]centroid, k)
 	for i := 0; i < k; i++ {
-		c[i] = centroid{x: rand.Float64(), y: rand.Float64()}
+		c[i] = centroid{x: rand.Float64() * maxX, y: rand.Float64() * maxY}
 	}
 	return c
 }
