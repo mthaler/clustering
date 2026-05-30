4ec00edb22ddadf04c3c746cfcf626b6ad87fda1
Michael Thaler <michael.thaler2@gmail.com>
2026-05-30 10:34:19 +0200

    Updated main.go.

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
 	"math"
 	"math/rand"
+	"os"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+}
+
+func readCsvFile(filePath string) [][]string {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Unable to read input file "+filePath, err)
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+	}
+
+	return records
 }
 
 // calculates the Euclidan distance between two points
