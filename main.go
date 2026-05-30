c1be7b29ae8ae60ea87777bce21cbb312bcd3c46
Michael Thaler <michael.thaler2@gmail.com>
2026-05-30 10:02:55 +0200

    Updated main.go.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 )
 
 func main() {
@@ -12,4 +13,14 @@ func main() {
 // calculates the Euclidan distance between two points
 func distance(p1 point, p2 point) float64 {
 	return math.Sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y))
+}
+
+// generates k centroids
+func generateCentroids(k int) []centroid {
+	var c []centroid
+	c = make([]centroid, k)
+	for i := 0; i < k; i++ {
+		c[i] = centroid{x: rand.Float64(), y: rand.Float64()}
+	}
+	return c
 }
