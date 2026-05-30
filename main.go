c029f48e59ac07eb115e295bed8bbfda431dacba
Michael Thaler <michael.thaler2@gmail.com>
2026-05-30 09:42:33 +0200

    Updated main.go and point.go.

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+}
+
+// calculates the Euclidan distance between two points
+func distance(p1 point, p2 point) float64 {
+	return math.Sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y))
 }
