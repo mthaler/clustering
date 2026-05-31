a2cc09d63a90f75910a49fb48a47f581fd17f1de
Michael Thaler <michael.thaler2@gmail.com>
2026-05-31 18:06:44 +0200

    Updated main_test.go.

--- main_test.go
+++ main_test.go
@@ -3,5 +3,10 @@ package main
 import "testing"
 
 func TestDistance(t *testing.T) {
-
+	p1 := point{x: 1, y: 0}
+	p2 := point{x: 2, y: 0}
+	d := distance(p1, p2)
+	if d != 1.0 {
+		t.Errorf("Distance is %g, should be 1", d)
+	}
 }
