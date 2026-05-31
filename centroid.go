eda59de84e67e61438c72cf3937fd483c66bde3e
Michael Thaler <michael.thaler2@gmail.com>
2026-05-31 17:32:43 +0200

    Updated centroid.go.

--- centroid.go
+++ centroid.go
@@ -4,3 +4,20 @@ type centroid struct {
 	x float64
 	y float64
 }
+
+func (c centroid) average(points []point) {
+	if len(points) == 0 {
+		return
+	}
+
+	x := 0.0
+	y := 0.0
+	for _, p := range points {
+		x += p.x
+		y += p.y
+	}
+	x /= float64(len(points))
+	y /= float64(len(points))
+	c.x = x
+	c.y = y
+}
